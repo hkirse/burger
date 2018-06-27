d2aadbfaa650e94dac9053bb0793e42724113e91
Henry Kirse <hkirse@gmail.com>
2018-06-26 21:49:18 -0400

    Adjusted Jaws for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,7 @@ var mysql = require("mysql");
 
 var connection;
 
-//creating JAWS for Heroku upload
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+
     connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -14,7 +10,7 @@ else {
     password: "DurhamPortland910",
     database: "burgers_db"
 });
-}
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected to database as id " + connection.threadId);
