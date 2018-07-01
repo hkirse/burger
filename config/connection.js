8da07fe060815e05124d0fb5f47171e0629e269d
Henry Kirse <hkirse@gmail.com>
2018-07-01 17:36:25 -0400

    Adding JAWS

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,12 @@ var mysql = require("mysql");
 
 var connection;
 
+//creating JAWS for Heroku upload
+if (process.env.JAWSDB_URL) {
+      connection = mysql.createConnection(process.env.JAWSDB_URL);
+    }
+
+    else {
 
     connection = mysql.createConnection({
     host: "ec2-184-73-175-95.compute-1.amazonaws.com",
@@ -10,6 +16,7 @@ var connection;
     password: "ae4138c1864fb9cf31155e94031065bb750774e0fe8a9d622e96ddc6645cb7a8",
     database: "df10ln5njchpki"
 });
+    }
 
 connection.connect(function(err) {
     if (err) throw err;
